425c332a78770056be0edda73b3396e564018c6c
djangulo <djal@tuta.io>
2019-07-29 18:13:40 -0400

    added staging deployment infrastructure

---
+++ compose/staging/_sourced/constants.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+SCRIPT_WARNING="This script provides the basic setting for a traefik-scalable config\n(with docker), it may make changes to the machine's filesystem."
+DOCKER_REQUIRED="This script requires docker to run: docker not found in \$PATH"
+SED_REQUIRED="This script requires GNU sed <https://www.gnu.org/software/sed/>: sed not found in \$PATH"
